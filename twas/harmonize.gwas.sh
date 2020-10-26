edfe738010af89f69bab883b9f762825f2dcf97d
poonchilam <43943114+chilampoon@users.noreply.github.com>
2020-10-26 12:38:35 +0800

    Change reference data to 1kG

--- twas/harmonize.gwas.sh
+++ twas/harmonize.gwas.sh
@@ -5,9 +5,9 @@ DATA=/NAS_lab/pclam/gwas
 OUTPUT=/NAS_lab/pclam/gwas/summary_stats/harmonized_gwas
 
 python $GWAS_TOOLS/gwas_parsing.py \
-	-gwas_file $DATA/summary_stats/cytokines/CRP.new.txt \
+	-gwas_file $DATA/summary_stats/cytokines/CRP.new.txt.gz \
 	-liftover $DATA/liftover/hg19ToHg38.over.chain.gz \
-	-snp_reference_metadata $DATA/snp_data/gtex_v8_eur_filtered_maf0.01_monoallelic_variants.txt.gz METADATA \
+	-snp_reference_metadata $DATA/reference_panel_1000G/variant_metadata.txt.gz METADATA \
 	-output_column_map SNP variant_id \
 	-output_column_map ALLELE0 non_effect_allele \
 	-output_column_map ALLELE1 effect_allele \
@@ -17,6 +17,6 @@ python $GWAS_TOOLS/gwas_parsing.py \
 	-output_column_map A1FREQ frequency \
 	-output_column_map CHR chromosome \
 	-output_column_map BP position \
-  --chromosome_format \
+  	--chromosome_format \
 	--insert_value sample_size 418642 \
-	-output $OUTPUT/crp.harmonised.txt
+	-output $OUTPUT/CRP.harmonised.txt.gz
