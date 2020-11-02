6d4c8bfe065d4427bf73ce3e7cd436805f76e1fc
poonchilam <43943114+chilampoon@users.noreply.github.com>
2020-11-02 18:11:15 +0800

    Fix Putamen name

--- twas/run.SPrediXcan.sh
+++ twas/run.SPrediXcan.sh
@@ -8,7 +8,7 @@ RESDIER=/home/pclam/tools/twas/results/cytokines
 for DATA in CRP IFNg IL6 MCP1
 do
   mkdir $RESDIER/$DATA
-  for t in Adipose_Visceral_Omentum Artery_Tibial Brain_Caudate_basal_ganglia Brain_Spinal_cord_cervical_c-1 Brain_Hypothalamus Brain_Putamen Whole_Blood
+  for t in Adipose_Visceral_Omentum Artery_Tibial Brain_Caudate_basal_ganglia Brain_Spinal_cord_cervical_c-1 Brain_Hypothalamus Brain_Putamen_basal_ganglia Whole_Blood
   do
     python $TOOL/SPrediXcan.py \
       --keep_non_rsid \
